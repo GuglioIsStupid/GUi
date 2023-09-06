601b9d1b29cbbeb3c453b2f4a5f7130ac9b45e76
GuglioIsStupid <bradsbluetab@gmail.com>
2023-09-06 13:22:09 -0400

    first commit

---
+++ GUi/init.lua
@@ -0,0 +1,7 @@
+local path = (...):gsub("%.init$", "") .. "."
+
+local GUi = {
+    _VERSION     = 'GUi v0.0.1', -- Pronounced "Guh I"
+    _DESCRIPTION = 'A simple GUI library for LÖVE',
+    _AUTHOR = "GuglioIsStupid"
+}
